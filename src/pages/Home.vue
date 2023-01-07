da4ef4e58030711e95ad86444bbf548a3e4eecc2
silk1111 <1461047853@qq.com>
2023-01-08 00:55:50 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	new file:   src/assets/styles/border.css 	new file:   src/assets/styles/iconfont.css 	new file:   src/assets/styles/iconfont/demo.css 	new file:   src/assets/styles/iconfont/demo_index.html 	new file:   src/assets/styles/iconfont/iconfont.js 	new file:   src/assets/styles/iconfont/iconfont.json 	new file:   src/assets/styles/iconfont/iconfont.ttf 	new file:   src/assets/styles/iconfont/iconfont.woff 	new file:   src/assets/styles/iconfont/iconfont.woff2 	new file:   src/assets/styles/reset.css 	new file:   src/assets/styles/themeColor.less 	deleted:    src/components/HelloWorld.vue 	modified:   src/main.js 	new file:   src/pages/Home.vue 	new file:   src/pages/components/Header.vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <HomeHeader></HomeHeader>
+    </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header.vue'
+ /* eslint-disable */
+export default {
+    name: 'Home',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+    components: {
+        HomeHeader
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
